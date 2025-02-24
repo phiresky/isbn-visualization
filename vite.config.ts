b7195ec6c67b6dccbadbec359ff9604a9b42020e
phiresky <phireskyde+git@gmail.com>
2025-02-24 19:49:37 +0100

    don't copy huge build dir

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
   base: process.env.BASE_PATH,
-});
+  // don't copy the (potentially huge) public directory to the build
+  publicDir: command === "serve" ? "public" : false,
+}));
